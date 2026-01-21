8f13d3b84c81ce95666b4b6d75fe09a5b9da2f68
DanushYallamilli <danushdatta333@gmail.com>
2026-01-21 20:01:03 +0530

    Time: 28 ms (27.63%) | Memory: 7.9 MB (61.84%) - LeetSync

--- 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.c
+++ 767-prime-number-of-set-bits-in-binary-representation/prime-number-of-set-bits-in-binary-representation.c
@@ -1,42 +1,27 @@
 int countPrimeSetBits(int left, int right)
-{
-    int w[right+1-left];
-    int a = left;
-    //assigning the given range to the list
-    for(int i = 0; i<right+1-left;i++)
+{int prime_count = 0;
+    
+    for(int i = left; i <= right; i++)
     {
-        w[i] = a++;
-    }
-    //Display
-    for(int i = 0; i<right+1-left; i++)
-    {
-        printf("%d ",w[i]);
-    }
-    int p[right+1-left];
-    for(int i = 0; i<right+1-left; i++)
-    {
-        if(w[i] == 0) p[i] = 0;
-        else
+        int count = 0;
+        int n = i;
+        
+        while(n != 0)
         {
-            int count = 0;
-            while(w[i] != 0)
+            if(n % 2 == 1)
             {
-                if(w[i] % 2 == 1)
-                {
-                    count++;
-                    p[i] = count;
-                }
-                w[i] /= 2;
+                count++;
             }
+            n /= 2;
         }
-    }
-    int prime = 0;
-    for(int i = 0; i<right+1-left; i++)
-    {
-        if(p[i] == 2 || p[i] == 3 || p[i] == 5 || p[i] == 7 || p[i] == 11 || p[i] == 13 || p[i] == 17 || p[i] == 19)
+
+        if(count == 2 || count == 3 || count == 5 || count == 7 || 
+           count == 11 || count == 13 || count == 17 || count == 19 || 
+           count == 23 || count == 29 || count == 31)
         {
-            prime++;
+            prime_count++;
         }
     }
-    return prime;
+    
+    return prime_count;
 }
