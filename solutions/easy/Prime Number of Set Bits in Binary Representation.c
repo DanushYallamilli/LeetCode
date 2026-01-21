503eabbd2ac4a3da106f62a574ff785e350b881a
DanushYallamilli <danushdatta333@gmail.com>
2026-01-21 19:56:42 +0530

    Add solution for Prime Number of Set Bits in Binary Representation

---
+++ solutions/easy/Prime Number of Set Bits in Binary Representation.c
@@ -0,0 +1,6 @@
+// Title: Prime Number of Set Bits in Binary Representation
+            // Difficulty: Easy
+            // Language: C
+            // Link: https://leetcode.com/problems/prime-number-of-set-bits-in-binary-representation/
+
+i
