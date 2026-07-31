0f7bca1c39ac65c1e2191c602ff2fb9fe4f9e586
Danush Datta Yallamilli <danushdatta333@gmail.com>
2026-07-31 15:00:46 +0530

    [1572] [Time Beats: 100.00%] [Memory Beats: 14.11%] - LeetPush

---
+++ 1572-matrix-diagonal-sum/1572-Matrix-Diagonal-Sum.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int diagonalSum(int[][] mat)
+    {
+        int m = mat.length;
+        int sum=0;
+
+        for(int i = 0; i < m; i++)
+        {
+            sum += mat[i][i];
+            sum += mat[m-1-i][i];
+        }
+
+        if(m%2 != 0)
+        {
+            sum -= mat[m/2][m/2];
+        }
+        return sum;
+    }
+}
